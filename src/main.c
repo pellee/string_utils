62e88126f236dd1f0f4bcf2303a18860f43bb26f
pelle <joaquinpellegrino@gmail.com>
2025-08-17 22:25:39 -0300

    add function to count vowels

--- src/main.c
+++ src/main.c
@@ -2,9 +2,10 @@
 #include <string.h>
 
 const char *DELIMITER = " ";
+const int VOWELS_QUAN = 5;
 
-void wordCount(char *str_original) {
-  const char *str_token = strtok(str_original, DELIMITER);
+void wordCount(char *str) {
+  const char *str_token = strtok(str, DELIMITER);
   int word_count = 0;
 
   while (str_token != NULL) {
@@ -15,12 +16,28 @@ void wordCount(char *str_original) {
   printf("word count: %d\n", word_count);
 }
 
+void vowelCount(const char *str) {
+  const char vowels[] = "aeiou";
+  int vowels_found = 0;
+
+  for (int i = 0; i < strlen(str); i++) {
+    for (int j = 0; j < VOWELS_QUAN; j++) {
+      if (str[i] == vowels[j])
+        vowels_found++;
+    }
+  }
+
+  printf("vowels found: %d\n", vowels_found);
+}
+
 int main(int argc, const char **argv) {
 
-  if (strcmp(argv[1], "-wc") != 0) {
+  if (strcmp(argv[1], "-wc") == 0)
+    wordCount((char *)argv[2]);
+  else if (strcmp(argv[1], "-vc") == 0)
+    vowelCount((char *)argv[2]);
+  else
     printf("command not found\n");
-    return 0;
-  }
-  wordCount((char *)argv[2]);
+
   return 0;
 }
