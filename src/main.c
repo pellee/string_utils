9ad48895d9ae2fbd6555f6b86f9d4b167d3b43b1
pelle <joaquinpellegrino@gmail.com>
2025-08-18 19:43:24 -0300

    add new funcionts for counting. Fix cases when the incoming string is in uppercase.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -16,13 +17,37 @@ void wordCount(char *str) {
   printf("word count: %d\n", word_count);
 }
 
+void charactersCount(const char *str) {
+  int chars_count = 0;
+  for (int i = 0; i < strlen(str); i++) {
+    if (str[i] != ' ')
+      chars_count++;
+  }
+  printf("characters found: %d\n", chars_count);
+}
+
+void consonantsCount(const char *str) {
+  const char consonants[] = "BCDFGHJKLMNPQRSTVWXYZ";
+  int consonants_found = 0;
+
+  for (int i = 0; i < strlen(str); i++) {
+    for (int j = 0; j < strlen(consonants); j++) {
+      if (toupper(str[i]) == consonants[j]) {
+        consonants_found++;
+      }
+    }
+  }
+
+  printf("consotants found: %d\n", consonants_found);
+}
+
 void vowelCount(const char *str) {
-  const char vowels[] = "aeiou";
+  const char vowels[] = "AEIOU";
   int vowels_found = 0;
 
   for (int i = 0; i < strlen(str); i++) {
     for (int j = 0; j < VOWELS_QUAN; j++) {
-      if (str[i] == vowels[j])
+      if (toupper(str[i]) == vowels[j])
         vowels_found++;
     }
   }
@@ -35,7 +60,11 @@ int main(int argc, const char **argv) {
   if (strcmp(argv[1], "-wc") == 0)
     wordCount((char *)argv[2]);
   else if (strcmp(argv[1], "-vc") == 0)
-    vowelCount((char *)argv[2]);
+    vowelCount(argv[2]);
+  else if (strcmp(argv[1], "-cc") == 0)
+    charactersCount(argv[2]);
+  else if (strcmp(argv[1], "-cnc") == 0)
+    consonantsCount(argv[2]);
   else
     printf("command not found\n");
 
